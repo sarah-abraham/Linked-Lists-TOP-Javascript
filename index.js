f51b2ee083a7fb2c2631806fe891c5f6498903d4
sarah-abraham <sarahabraham2014@gmail.com>
2024-07-16 14:14:12 +0530

    Created node and linked list classes with contructors and added append function to the linked list

---
+++ index.js
@@ -0,0 +1,27 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    append(value){
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let temp = this.head;
+            while (temp.next !== null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        this.size++;
+    }
+}
